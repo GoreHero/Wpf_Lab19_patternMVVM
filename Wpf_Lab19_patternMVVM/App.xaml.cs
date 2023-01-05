ab8b342a1ec27a3338a96ba5a55546ab12c6bc71
Asus <Asus@CIRKA>
2023-01-06 00:03:21 +0300

    Добавьте файлы проекта.

---
+++ Wpf_Lab19_patternMVVM/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_Lab19_patternMVVM
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Wpf_Lab19_patternMVVM/Models/Сircumference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Lab19_patternMVVM.Models
+{
+    internal class Сircumference
+    {
+        public static double GetLength(double a) => Math.Abs(2 * Math.PI * a);
+    }
+}
